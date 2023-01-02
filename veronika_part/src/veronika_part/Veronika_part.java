69592ee6896a60af68ba5035dacb9d60b46d41eb
veronikasnigur <93828479+veronikasnigur@users.noreply.github.com>
2023-01-02 18:08:50 +0200

    GUI used in NetBeans

---
+++ veronika_part/src/veronika_part/Veronika_part.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package veronika_part;
+
+/**
+ *
+ * @author veronika_snigur
+ */
+public class Veronika_part {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Log().setVisible(true);
+    }
+    
+}
